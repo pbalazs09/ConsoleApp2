1741c199c5c3fdb30124d713ca5ff66c9576822b
Pálfi Balázs <pbalazs09@gmail.com>
2020-04-23 14:25:21 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Combo v = new Combo(5000, 7500, 2500);
+            v.exponencialis();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Valszam.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class WithoutLambda
+    {
+        private double lambda;
+        private double ex;
+        private double t;
+
+        public WithoutLambda(double _ex, double _t)
+        {
+            t = _t;
+            ex = _ex;
+            lambda = 1 / _ex;
+        }
+        
+        public void exponencialis()
+        {
+            double ft = 1 - Math.Pow(Math.E, -lambda * t);
+            Console.WriteLine("Valószínűség, ha hamarabb: " + ft);
+            Console.WriteLine("Valószínűség, ha később: " + (1 - ft));
+        }
+    }
+
+    class WithLambda
+    {
+        private double lambda;
+        private double ex;
+        private double t;
+
+        public WithLambda(double _lambda, double _t)
+        {
+            t = _t;
+            lambda = _lambda;
+            ex = 1 / lambda;        
+        }
+
+        public void exponencialis()
+        {
+            double ft = 1 - Math.Pow(Math.E, -lambda * t);
+            Console.WriteLine("Valószínűség, ha hamarabb: " + ft);
+            Console.WriteLine("Valószínűség, ha később: " + (1 - ft));
+        }
+    }
+
+    class Combo
+    {
+        private double lambda;
+        private double ex;
+        private double t1;
+        private double t2;
+
+        public Combo(double _ex, double _t1, double _t2)
+        {
+            t1 = _t1;
+            t2 = _t2;
+            ex = _ex;
+            lambda = 1 / _ex;
+        }
+
+        public void exponencialis()
+        {
+            double ft1 = 1 - Math.Pow(Math.E, -lambda * t1);
+            double ft2 = 1 - Math.Pow(Math.E, -lambda * t2);
+            Console.WriteLine("Valószínűség: " + (ft1 - ft2));
+        }
+    }
+}
